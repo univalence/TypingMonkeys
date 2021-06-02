fa6d914181c84e046156ccf8bc9ed9049f9e3371
guihardbastien <bastien_guihard@orange.fr>
2021-06-02 17:45:07 +0200

    Feat: Settings

--- src/monkey_shell/components/core.clj
+++ src/monkey_shell/components/core.clj
@@ -1,15 +1,38 @@
 (ns monkey-shell.components.core)
 
+(defn vbox
+  "Vbox wrapper"
+  [component-vector]
+  {:fx/type  :v-box
+   :padding  20
+   :spacing  10
+   :children component-vector})
+
+(defn hbox
+  "Hbox wrapper"
+  [component-vector]
+  {:fx/type  :h-box
+   :padding  20
+   :spacing  10
+   :children component-vector})
+
+(defn squared-btn
+  "squared button that return its name on click"
+  [text on-action-event-keyword]
+  {:fx/type    :button
+   :text       text
+   :pref-width 150
+   :on-action  {:event/type    on-action-event-keyword
+                :click-payload text}})
+
 (defn text-entry
   "Text bar with enter button"
-  [on-text-change-event-keyword on-action-event-keyword]
-  {:fx/type  :h-box
-   :children [{:fx/type         :text-field
-               :on-text-changed {:event/type on-text-change-event-keyword}}
-              {:fx/type    :button
-               :text       "ENTER"
-               :pref-width 100
-               :on-action  {:event/type on-action-event-keyword}}]})
+  ([on-text-change-event-keyword on-action-event-keyword]
+   (text-entry on-text-change-event-keyword on-action-event-keyword "ENTER"))
+  ([on-text-change-event-keyword on-action-event-keyword text]
+   (hbox [{:fx/type         :text-field
+           :on-text-changed {:event/type on-text-change-event-keyword}}
+          (squared-btn text on-action-event-keyword)])))
 
 (defn text-thread
   "Chronologically ordered text.
@@ -17,33 +40,42 @@
   TODO print full history "
   [state]
   {:fx/type      :scroll-pane
-   :pref-width 400
-   :pref-height 400
+   :pref-width   400
+   :pref-height  400
    :v-box/vgrow  :always
    :fit-to-width true
    :content      {:fx/type  :v-box
                   :children [{:fx/type :text
-                              :text    (-> (get-in state [:shell-sessions (keyword (get-in state [:session :id])) :history  ])
+                              :text    (-> (get-in state [:shell-sessions (keyword (get-in state [:session :id])) :history])
                                            first
                                            :result
                                            :out
                                            str)}]}})
 
-(defn squared-btn
-  "squared button that return its name on click"
-  [text on-action-event-keyword]
-  {:fx/type  :h-box
-   :spacing  5
-   :padding  5
-   :children [{:fx/type    :button
-               :text       text
-               :pref-width 150
-               :on-action  {:event/type on-action-event-keyword
-                            :click-payload text}}]})
-
 (defn sidebar
   "Menu-like component (list of buttons)"
   [on-action-event-keyword btn-list]
-  {:fx/type  :v-box
-   :children (mapv #(squared-btn % on-action-event-keyword) btn-list)})
+  (vbox (mapv #(squared-btn % on-action-event-keyword) btn-list)))
 
+(defn window
+  "Window component"
+  [root-component]
+  {:fx/type       :stage
+   :always-on-top true
+   :showing       true
+   :scene         {:fx/type :scene
+                   :root    root-component}})
+
+(defn radio-btn
+  "Single radio button,
+  can be used in radio group"
+  [selected? on-action-event-keyword text]
+  {:fx/type  :radio-button
+   :selected selected?
+   :text     text})
+
+(defn radio-group
+  "Vertically organized radio buttons"
+  [id->true?]
+  (vbox
+    (mapv #(radio-btn (last %) :fixme (first %)) id->true?)))
--- src/monkey_shell/containers/shell_opts.clj
+++ src/monkey_shell/containers/shell_opts.clj
@@ -10,15 +10,28 @@
                    :session        {:id "first-shell"}
 
                    :shell-sessions {:first-shell  {:history []
-                                                   :members []}
+                                                   :members ["bastien@univalence.io" "pierre@univalence.io"]}
                                     :second-shell {:history []
-                                                   :members []}}}))
+                                                   :members ["bastien@univalence.io" "francois@univalence.io"]}}}))
+
+(defn members->true []
+  (zipmap (get-in @*state
+                  [:shell-sessions (keyword (get-in @*state [:session :id]))
+                   :members]) (repeat true)))
+
+(defn add-member [member-id]
+  (swap! *state
+         (fn [state]
+           (update-in state
+                      [:shell-sessions (keyword (get-in state [:session :id])) :members]
+                      conj [member-id]))))
 
 (defn create-session! [session-name]
   (swap! *state
          (fn [state]
-           (assoc-in state [:shell-sessions (keyword session-name)] {:history []
-                                                                     :members []}))))
+           (assoc-in state [:shell-sessions (keyword session-name)]
+                     {:history []
+                      :members []}))))
 (defn execute!
   "TODO DOC"
   [shell-session]
@@ -28,20 +41,37 @@
            (let [cmd-args (str/split (:input state) #" ")
                  result (apply shell/sh cmd-args)]
 
-             (update-in state [:shell-sessions (keyword shell-session) :history] conj {:cmd-args cmd-args
-                                                                                       :result   result})))))
+             (update-in state
+                        [:shell-sessions (keyword shell-session) :history]
+                        conj {:cmd-args cmd-args
+                              :result   result})))))
 
-(defn handler [{:keys [id fx/event]}]
-  (swap! *state assoc :module (or id (keyword event))))
+#_(defn handler [{:keys [id fx/event]}]
+    (swap! *state assoc :module (or id (keyword event))))
 
-(defn map-event-handler [event]
-  #_(println (get event :session-id))
-  (case (:event/type event)
-    :capture-text (swap! *state assoc :input (get event :fx/event))
-    :execute (do (execute! (get-in @*state [:session :id])))
-    :handle-sidebar-click (swap! *state assoc-in [:session :id] (get event :click-payload))
-    :create-session (println "TODO create session")
-    ))
+#_(defn open-settings []
+  (fx/on-fx-thread
+    (fx/create-component
+      {:fx/type fx/ext-many
+       :desc    [(ui/window
+                   (ui/vbox [(ui/radio-group (members->true))
+                             (ui/text-entry :capture-new-member-text :add-member "Add member")
+                             (ui/squared-btn "OK" :mock)]))]})))
+
+(defn member-select [_]
+  {:fx/type fx/ext-many
+   :desc    [(ui/window
+               (ui/vbox [(ui/radio-group (members->true))
+                         (ui/text-entry :capture-new-member-text :add-member "Add member")
+                         (ui/squared-btn "OK" :mock)]))]})
+
+(declare handler)
+
+(def settings-renderer
+  (fx/create-renderer
+    :middleware (fx/wrap-map-desc assoc :fx/type member-select)
+    :opts {:fx.opt/map-event-handler handler}))
+
 
 (defn root [state] {:fx/type :stage
                     :showing true
@@ -49,19 +79,35 @@
                     :height  600
                     :scene   {:fx/type :scene
                               :root    {:fx/type  :v-box
-                                        :children [(ui/squared-btn "✚" :create-session)
+                                        :children [(ui/text-entry :capture-new-room-text :create-session "Add Session")
                                                    {:fx/type  :h-box
                                                     :children [(ui/sidebar :handle-sidebar-click
                                                                            (keys (walk/stringify-keys
                                                                                    (get state :shell-sessions))))
                                                                {:fx/type  :v-box
                                                                 :children [(ui/text-thread state)
-                                                                           (ui/text-entry :capture-text :execute)]}]}]}}})
+                                                                           (ui/text-entry :capture-text :execute)
+                                                                           (ui/squared-btn (str (get-in @*state [:session :id]) "'s settings") :open-settings)]}]}]}}})
+
+(defn handler
+  "HANDLER"
+  [event]
+  #_(println (get event :session-id))
+  (case (:event/type event)
+    :capture-text (swap! *state assoc :input (get event :fx/event))
+    :execute (do (execute! (get-in @*state [:session :id])))
+    :handle-sidebar-click (swap! *state assoc-in [:session :id] (get event :click-payload))
+    :capture-new-room-text (swap! *state assoc :room-to-create (get event :fx/event))
+    :create-session (create-session! (get @*state :room-to-create))
+    :open-settings (settings-renderer {})
+    :capture-new-member-text (swap! *state assoc :member-to-add (get event :fx/event))
+    :add-member (add-member (get @*state :member-to-add))
+    :mock (println "TODO")))
 
 (fx/mount-renderer
   *state
   (fx/create-renderer
     :middleware (fx/wrap-map-desc assoc :fx/type root)
-    :opts {:fx.opt/map-event-handler map-event-handler}))
+    :opts {:fx.opt/map-event-handler handler}))
 
 (println @*state)
