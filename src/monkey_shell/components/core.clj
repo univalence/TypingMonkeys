32f17a7c95918a8de24d78a16e42b33a5f403f33
guihardbastien <bastien_guihard@orange.fr>
2021-06-02 12:44:56 +0200

    Feat data structure

--- src/monkey_shell/components/core.clj
+++ src/monkey_shell/components/core.clj
@@ -17,11 +17,13 @@
   TODO print full history "
   [state]
   {:fx/type      :scroll-pane
+   :pref-width 400
+   :pref-height 400
    :v-box/vgrow  :always
    :fit-to-width true
    :content      {:fx/type  :v-box
                   :children [{:fx/type :text
-                              :text    (-> (get-in state [:histories (keyword (get-in state [:shell-session :id]))])
+                              :text    (-> (get-in state [:shell-sessions (keyword (get-in state [:session :id])) :history  ])
                                            first
                                            :result
                                            :out
@@ -29,15 +31,15 @@
 
 (defn squared-btn
   "squared button that return its name on click"
-  [session-id on-action-event-keyword]
+  [text on-action-event-keyword]
   {:fx/type  :h-box
    :spacing  5
    :padding  5
    :children [{:fx/type    :button
-               :text       session-id
+               :text       text
                :pref-width 150
                :on-action  {:event/type on-action-event-keyword
-                            :session-id session-id}}]})
+                            :click-payload text}}]})
 
 (defn sidebar
   "Menu-like component (list of buttons)"
--- src/monkey_shell/containers/shell_opts.clj
+++ src/monkey_shell/containers/shell_opts.clj
@@ -5,27 +5,31 @@
             [clojure.java.shell :as shell]
             [monkey-shell.components.core :as ui]))
 
+(def *state (atom {:user           {:id "bastien@univalence.io"}
+                   :input          nil
+                   :session        {:id "first-shell"}
 
-(def *state (atom {:history       ()
-                   :sessions      (list "first-shell" "second-shell" "third-shell")
-                   :shell-session {:id "first-shell"}
-                   :histories     {:first-shell  ()
-                                   :second-shell ()
-                                   :third-shell  ()}}))
+                   :shell-sessions {:first-shell  {:history []
+                                                   :members []}
+                                    :second-shell {:history []
+                                                   :members []}}}))
 
+(defn create-session! [session-name]
+  (swap! *state
+         (fn [state]
+           (assoc-in state [:shell-sessions (keyword session-name)] {:history []
+                                                                     :members []}))))
 (defn execute!
   "TODO DOC"
   [shell-session]
-  #_(println(get-in @*state [:histories (keyword shell-session)]))
+  #_(println (get-in @*state [:histories (keyword shell-session)]))
   (swap! *state
          (fn [state]
-           (let [cmd-args (str/split (:text state) #" ")
+           (let [cmd-args (str/split (:input state) #" ")
                  result (apply shell/sh cmd-args)]
 
-             (update-in state [:histories (keyword shell-session)] conj {:cmd-args cmd-args
-                                                                     :result   result})
-
-             ))))
+             (update-in state [:shell-sessions (keyword shell-session) :history] conj {:cmd-args cmd-args
+                                                                                       :result   result})))))
 
 (defn handler [{:keys [id fx/event]}]
   (swap! *state assoc :module (or id (keyword event))))
@@ -33,21 +37,26 @@
 (defn map-event-handler [event]
   #_(println (get event :session-id))
   (case (:event/type event)
-    :capture-text (swap! *state assoc :text (get event :fx/event))
-    :execute (do (execute! (get-in @*state [:shell-session :id]) ))
-    :handle-sidebar-click (swap! *state assoc-in [:shell-session :id] (get event :session-id))
+    :capture-text (swap! *state assoc :input (get event :fx/event))
+    :execute (do (execute! (get-in @*state [:session :id])))
+    :handle-sidebar-click (swap! *state assoc-in [:session :id] (get event :click-payload))
+    :create-session (println "TODO create session")
     ))
 
 (defn root [state] {:fx/type :stage
                     :showing true
                     :width   600
-                    :height  200
+                    :height  600
                     :scene   {:fx/type :scene
-                              :root    {:fx/type  :h-box
-                                        :children [(ui/sidebar :handle-sidebar-click (get state :sessions))
-                                                   {:fx/type  :v-box
-                                                    :children [(ui/text-thread state)
-                                                               (ui/text-entry :capture-text :execute)]}]}}})
+                              :root    {:fx/type  :v-box
+                                        :children [(ui/squared-btn "✚" :create-session)
+                                                   {:fx/type  :h-box
+                                                    :children [(ui/sidebar :handle-sidebar-click
+                                                                           (keys (walk/stringify-keys
+                                                                                   (get state :shell-sessions))))
+                                                               {:fx/type  :v-box
+                                                                :children [(ui/text-thread state)
+                                                                           (ui/text-entry :capture-text :execute)]}]}]}}})
 
 (fx/mount-renderer
   *state
