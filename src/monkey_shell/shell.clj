8c0e0f5a62ec2cca674d8defc14dc64014c825b1
pbaille <pierrebaille@gmail.com>
2021-06-15 16:58:27 +0200

    WIP: shell state persistence

--- src/monkey_shell/shell.clj
+++ src/monkey_shell/shell.clj
@@ -1,5 +1,5 @@
 (ns monkey-shell.shell
-  (:require [babashka.process :as pc :refer [check process pb pipeline]]
+  (:require [babashka.process :as pc :refer [$ check process pb pipeline]]
             [clojure.string :as str]
             [clojure.java.io :as io]))
 
@@ -15,8 +15,7 @@
                        {:out (callback-writer
                                (fn [out]
                                  (on-out out)
-                                 (future #_(Thread/sleep 1000)
-                                         (when-not (.isAlive (:proc @p))
+                                 (future (when-not (.isAlive (:proc @p))
                                            (on-dead)))))}))))
 
 (defn context []
@@ -33,8 +32,55 @@
   ([user dir]
    (str user ":" dir ">")))
 
+(def PERSISTED_ENV_KEYS
+  ["PWD" "USER" "LOGNAME"])
+
+(defn wrap-cmd [s]
+  ["bash" "-c" (str "DIR=$PWD && " s " && cd $DIR && env > env.txt")])
+
+(defn env-str->clj [s]
+  (into {} (map (fn [x] (vec (next (re-matches #"(.*)=(.*)" x))))
+        (clojure.string/split s #"\n"))))
+
+(defn execute2
+  ([cmd]
+   (execute2 cmd println pr))
+  ([cmd on-out on-dead]
+   (let [p (process (wrap-cmd cmd)
+                    {:out (callback-writer
+                            (fn [out]
+                              (on-out out)))})]
+     (future @p (on-dead (env-str->clj (slurp "env.txt")))))))
+
+
+
+
 (comment
-  (deref (process '[cd src && pwd] {:out :string}))
+
+  (execute2 "cd src && pwd"
+            println
+            (comp println count keys))
+
+  (select-keys (.environment (:pb (pb '[])))
+               ["PWD" "USER" "LOGNAME"])
+
+  (deref (process '[env] {:out :string}))
   (clojure.java.shell/sh "pwd")
   (clojure.java.shell/sh "echo" "pouet")
-  (do clojure.java.shell/*sh-dir*))
+  (clojure.java.shell/sh "bash" "-c" "cd src && ls -la")
+  (clojure.java.shell/sh "bash" "-c" "export RETURN=$(cd src && ls -la && echo pouet) && env >> env.txt && echo $RETURN")
+  (clojure.java.shell/sh "bash" "-c" "cd src && ls -la && env >> env.txt")
+  (deref (process ["bash" "-c" "cd src && ls -la && env >> env.txt"] {:inherit true}))
+  ((pb ["bash" "-c" "cd src && ls -la"]))
+  (execute ["bash" "-c" "cd src && ls -la"]
+           (fn [_] (println (System/getenv "PWD")))
+           (fn [_]))
+  (deref (process ["bash" "-c" "cd src && ls -la"] {:out :string}))
+  (do clojure.java.shell/*sh-dir*)
+
+  (def p (pb '[]))
+  (pc/start p)
+  (do p)
+  (process '[echo yo]))
+
+()
