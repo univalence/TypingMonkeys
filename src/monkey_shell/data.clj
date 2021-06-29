c01d496647fb0c5c4fc1e36fef289d5391019c67
pbaille <pierrebaille@gmail.com>
2021-06-29 11:05:57 +0200

    chore: remove useless comment and code

--- src/monkey_shell/data.clj
+++ src/monkey_shell/data.clj
@@ -15,13 +15,10 @@
                (vec (remove #(= cmd-id (:id %)) cmds)))))
 
 (defn focused-session
-  "TODO move to DATA"
   [state]
   (get-in state [:shell-sessions (:focused-session state)]))
 
 (defn members->true [state]
-  "TODO : MOVE TO \"DATA\" NAMESPACE
-  FIXME : not working"
   (as-> (focused-session state) _
         (get _ :members)
         (map :db/id _)
