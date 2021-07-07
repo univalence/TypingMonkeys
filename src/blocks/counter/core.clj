7f275b691df21e423c7cbb2f88a61c99ab55630e
pbaille <pierrebaille@gmail.com>
2021-07-07 10:58:04 +0200

    feature : composite-ref

--- src/blocks/counter/core.clj
+++ src/blocks/counter/core.clj
@@ -6,30 +6,60 @@
 
 ; ---- fx machinery ----
 
-(def *state (sync/initialize-document "scratch/compteur" {:count 0}))
-
 (defn counter [state]
   (comps/hbox [(comps/squared-btn {:text "+"} :increment)
                {:fx/type :label
-                :text    (str (get state :count))}]))
+                :text (str (get state :count))}]))
 
-(defn root [state] {:fx/type :stage
-                    :showing true
-                    :scene   {:fx/type :scene
-                              :root    {:fx/type  :v-box
-                                        :children [(counter state)]}}})
 
-(defn handler
-  [event]
-  (case (:event/type event)
-    :increment (sync/swap! *state update :count inc)))
+
+(comment :ex1
+
+         (def *state (sync/initialize-document "scratch/compteur" {:count 0}))
+
+         (defn root [state]
+           {:fx/type :stage
+            :showing true
+            :scene {:fx/type :scene
+                    :root {:fx/type :v-box
+                           :children [(counter state)]}}})
+
+         (defn handler
+           [event]
+           (case (:event/type event)
+             :increment (sync/swap! *state update :count inc)))
+
+         (fx/mount-renderer
+           *state
+           (fx/create-renderer
+             :middleware (fx/wrap-map-desc assoc :fx/type root)
+             :opts {:fx.opt/map-event-handler handler})))
+
+(defonce c1 (sync/create-document "scratch/compteur-1" {:count 0}))
+(defonce c2 (sync/create-document "scratch/compteur-2" {:count 0}))
+(defonce c3 (sync/create-document "scratch/compteur-3" {:count 0}))
+
+(def *state
+  (sync/ref-map {:foo c1
+                 :bar c2
+                 :baz c3}))
+
+(defn named-counter [[name c]]
+  (comps/hbox [{:fx/type :label :text (str name)}
+               (counter c)]))
+
+(defn counter-map [state]
+  (comps/vbox (mapv named-counter state)))
+
+(defn root [state]
+  {:fx/type :stage
+   :showing true
+   :scene {:fx/type :scene
+           :root {:fx/type :v-box
+                  :children [(counter-map state)]}}})
 
 (fx/mount-renderer
   *state
   (fx/create-renderer
     :middleware (fx/wrap-map-desc assoc :fx/type root)
-    :opts {:fx.opt/map-event-handler handler}))
-
-
-
-
+    :opts {:fx.opt/map-event-handler (fn [_])}))
--- src/blocks/state_sync.clj
+++ src/blocks/state_sync.clj
@@ -1,4 +1,3 @@
-
 (ns blocks.state-sync
   (:refer-clojure :exclude [swap!])
   (:require [clojure.core :as core]
@@ -7,12 +6,13 @@
             [firestore-clj.core :as f]
             [typing-monkeys.utils.firestore :as fu]
             [manifold.stream :as st])
-  (:import (java.util HashMap ArrayList)))
+  (:import (java.util HashMap ArrayList)
+           (clojure.lang IDeref)))
 
 ; ---- firebase machinery ----
 
 (defonce db
-         (f/client-with-creds "data/unsafe.json"))
+  (f/client-with-creds "data/unsafe.json"))
 
 (defn pull-walk
 
@@ -51,18 +51,26 @@
         new-state (apply core/swap! atom f args)]
     (f/set! ref (clojure.walk/stringify-keys new-state))))
 
+(defn existing-ref?
+  [ref]
+  (seq (f/pull ref)))
 
+(defn connect-document
 
-(defn ???
+  "connect to a firebase document
+   returns an atom representing it and synchronized on it
+   throws if the document do not exist"
+
   [id]
 
   (let [ref (f/doc db id)
 
-        _ (assert (seq (f/pull ref))
+        _ (assert (existing-ref? ref)
                   "non existant document,
                    please consider using 'initialize-document")
 
-        *m (atom (pull-walk ref))]
+        *m (with-meta (atom (pull-walk ref))
+                      {:db/ref ref})]
 
     (watch! ref
             (fn [x]
@@ -70,15 +78,15 @@
                 (core/reset! *m x))))
     *m))
 
-(defn initialize-document
+(defn create-document
 
   "create a firestore document with the given id and data
    returns an atom and setup the synchronisation between it and firestore"
 
   [id data]
 
-  (let [*m (atom data)
-        ref (f/doc db id)]
+  (let [ref (f/doc db id)
+        *m (with-meta (atom data) {:db/ref ref})]
 
     (f/set! ref (clojure.walk/stringify-keys data))
 
@@ -88,26 +96,167 @@
                 (core/reset! *m x))))
     *m))
 
+(defn bind-document
+
+  "the union of connect-document and initialize-document
+   if the given id do not point to an existing document,
+   default-data is used to create a new one"
+
+  [id default-data]
+  (let [ref (f/doc db id)]
+    (if (existing-ref? ref)
+      (connect-document id)
+      (create-document id default-data))))
+
 (comment :exemple
+         (def my-data
+           (bind-document
+             "scratch/my-data"
+             {:name "papa" :destination "maman" :score 0}))
+         (swap! my-data update :score inc)
+         (assert (= 1 (:score @my-data))))
 
-    (def my-data
-      (initialize-document
-        "scratch/my-data"
-        {:name "papa" :destination "maman" :score 0}))
+(do :composition
 
-    (swap! my-data update :score inc)
+    (comment
+      (def m {:a 1 :b {:c 2}})
 
-    (assert (= 1 (:score @my-data))))
+      {:get (fn [m] (get m :a))
+       :set (fn [m f] (update m :a f))}
 
+      {:get (fn [m] (get-in m [:b :c]))
+       :set (fn [m f] (update-in m [:b :c] f))})
 
-; ---- utils ----
+    (comment :proto-and-records
+             (defprotocol IShow
+               (show [_]))
 
-(defn document-id->atom
-  "usage: (document-id->atom \"scratch/counter1\")"
-  [document-id]
-  (-> (f/doc db document-id)
-      (pull-walk)
-      (atom)))
+             (defrecord Pair [left right]
+               IShow
+               (show [_] (list 'pair left right)))
+
+             (show (Pair. 1 2))
+             ;; => (pair 1 2)
+             )
+
+    (defrecord AtomLens [data get set]
+      clojure.lang.IDeref
+      (deref [_] (get data))
+      clojure.lang.IAtom
+      (swap [_ f] (->> (get data) f (set data)))
+      (swap [_ f x] (core/swap! _ #(f % x)))
+      (swap [_ f x y] (core/swap! _ #(f % x y)))
+      (swap [_ f x y zs] (core/swap! _ #(apply f % x y zs)))
+      )
+
+    (defmethod print-method AtomLens [this w]
+      (print-method (list 'atom-lens (:data this)) w))
+
+    (defn atom-lens-class [get set]
+      (fn [data]
+        (AtomLens. data get set)))
+
+    (def ref-map
+      (atom-lens-class
+        (fn [m] (into {} (map (fn [[k v]] [k (deref v)]) m)))
+        (fn [m value] (mapv #(reset! (m %) (value %)) (keys m)))))
+
+    (def c0 (atom {:count 0}))
+    (def c1 (atom {:count 1}))
+    (def c2 (atom {:count 2}))
+
+    (comment :ex1
+
+             (def gc (ref-map {:c1 c0 :c2 c1 :c3 c2}))
+
+             (assert (= (deref gc)
+                        {:c1 {:count 0} :c2 {:count 1} :c3 {:count 2}}))
+
+             (core/swap! gc update-in [:c2 :count] inc)
+
+             (assert (= (deref gc)
+                        {:c1 {:count 0}, :c2 {:count 2}, :c3 {:count 2}}))
+
+             (assert (= 2 (:count @c1))))
+
+    (comment :ex2
+             (def ref-vec
+               (atom-lens-class
+                 (fn [v] (mapv deref v))
+                 (fn [v value] (mapv #(reset! (get v %) (get value %)) (range (count v))))))
+
+             (ref-vec [c0 c1 c2])
+
+             (assert (= @(ref-vec [c0 c1 c2])
+                        [{:count 0} {:count 1} {:count 2}]))
+
+             (assert (= (core/swap! (ref-vec [c0 c1 c2]) update-in [0 :count] inc)
+                        [{:count 1} {:count 1} {:count 2}]))
+             )
+
+    (defn map-vals
+      "(= (map-vals inc {:a 1 :b 2})
+          {:a 2 :b 3})"
+      [f m]
+      (zipmap (keys m) (map f (vals m))))
+
+    (defn atom? [x]
+      (instance? clojure.lang.Atom x))
+
+    (def composite-ref
+      (atom-lens-class
+        (fn self [data]
+          (cond
+            (map? data) (map-vals self data)
+            (vector? data) (mapv self data)
+            (atom? data) (deref data)
+            :else data))
+
+        (fn self [data value]
+          (cond
+            (map? data) (merge-with self data value)
+            (vector? data) (mapv self data value)
+            (atom? data) (reset! data value)
+            :else value))))
+
+    (deref (ref-coll 12))
+
+    (comment :ex3
+             (def rc (composite-ref
+                       {:a c0
+                        :b [c1 c2]
+                        :c 12}))
+
+             (assert (= (deref rc)
+                        {:a {:count 0}
+                         :b [{:count 1} {:count 2}]
+                         :c 12}))
+
+             (assert (= (core/swap! rc update-in [:b 1 :count] inc)
+                        {:a {:count 0}
+                         :b [{:count 1} {:count 3}]
+                         :c 12}))
+
+             (assert (= {:count 3} @c2))
+
+             (core/swap! c2 update :count inc)
+
+             (deref rc)
+             ;; => {:a {:count 0}, :b [{:count 1} {:count 5}], :c 12}
+             )
+
+    )
+
+(comment :maybe-useless
+         (defn document-id->atom
+
+           "usage: (document-id->atom \"scratch/counter1\")"
+
+           [document-id]
+           (-> (f/doc db document-id)
+               (pull-walk)
+               (atom))))
+
 
 
 
